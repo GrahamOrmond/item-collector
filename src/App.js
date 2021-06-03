8c6516a37a7602e1b46e83ffd2a394691c3b8b09
GrahamOrmond <grahamwormond@gmail.com>
2021-06-03 04:21:12 -0400

    added project files

---
+++ src/App.js
@@ -0,0 +1,21 @@
+/*global chrome*/
+
+import React, { Component } from 'react';
+import './style.css';
+
+class App extends Component {
+
+    constructor(props){
+      super(props);
+    }
+
+    render() {
+
+      return (
+        <div className="app-body">
+              hello world
+        </div>
+      );
+    }
+}
+export default App;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+/*global chrome*/
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
